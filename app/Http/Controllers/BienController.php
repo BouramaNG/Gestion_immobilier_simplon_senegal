b687153d7f84087b51a997d9515749a55b133d69
benbasse <moussabasse221@gmail.com>
2023-11-19 01:07:59 +0000

    This is my controller 'Bien'

--- app/Http/Controllers/BienController.php
+++ app/Http/Controllers/BienController.php
@@ -2,17 +2,96 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Bien;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class BienController extends Controller
 {
-    public function AjoutBien()
+    public function index()
     {
         return view("admin.ajoutbien");
     }
-
-    public function ListeBien()
+    public function show ()
     {
-return view("admin.listebien");
+        $bien = Bien::all();
+        return view("admin.listebien", compact("bien"));
     }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            "nom"=> "required|string|min:3",
+            "categorie"=> "required|string",
+            "image"=> "required|image|max:5000",
+            "description"=> "required|string|min:5",
+            "addresse"=> "required|string",
+            "status"=> "required|string",
+            "date"=>"required|date",
+        ]);
+        $bien = new Bien();
+        $bien->nom = $request->nom;
+        $bien->categorie = $request->categorie;
+        // $bien->image = $request->image;
+        $bien->image = $this->storeImage($request->file('image'));
+        $bien->description = $request->description;
+        $bien->addresse = $request->addresse;
+        $bien->status = $request->status;
+        $bien->date = $request->date;
+        $bien->save();
+        return back()->with('success','Votre produit a été ajouter');
+        
+    }
+    private function storeImage($image): string
+    {
+        return $image->store('avatars', 'public');
+    }
+
+    public function delete($id)
+    {
+        $bien = Bien::find($id);
+        $bien->destroy($id);
+        if ($bien->save())
+        {
+            return back()->with('success','vous avez supprimer cette produit');
+        }
+    }
+
+
+    public function edit($id)
+    {
+        $bien = Bien::where(
+            'id', '=', $id
+        )->first();
+        // $bien = Bien::find($id);
+        return view("admin.modifier", compact("bien"));
+    }
+
+    public function update(Request $request, $id)
+{
+    $request->validate([
+        "nom" => "required|string|min:3",
+        "categorie" => "required|string",
+        "image" => "image|max:5000",
+        "description" => "required|string|min:5",
+        "addresse" => "required|string",
+        "status" => "required|string",
+        "date" => "required|date",
+    ]);
+
+    $bien = Bien::find($id);
+    $bien->nom = $request->nom;
+    $bien->categorie = $request->categorie;
+    if ($request->hasFile('image')) {
+        $bien->image = $this->storeImage($request->file('image'));
+    }
+    $bien->description = $request->description;
+    $bien->addresse = $request->addresse;
+    $bien->status = $request->status;
+    $bien->date = $request->date;
+    $bien->save();
+    // dd($bien);
+    return back()->with('success','Mise à jour réussi');
+}
+
 }
