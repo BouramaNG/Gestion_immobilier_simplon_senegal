795c54723e14ea8498fdb5bc54859dfcca85087d
benbasse <moussabasse221@gmail.com>
2023-11-25 12:17:46 +0000

    Ajouter des nouvelles methodes pour chambre

--- app/Http/Controllers/BienController.php
+++ app/Http/Controllers/BienController.php
@@ -2,10 +2,7 @@
 
 namespace App\Http\Controllers;
 
-
-
-
-
+use App\Models\Chambre;
 use App\Models\Multi_img;
 use App\Models\Propertie;
 use Illuminate\Http\Request;
@@ -19,29 +16,30 @@ use Illuminate\Support\Facades\Auth;
 
 class BienController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        // $nombre_de_chambres = $request->input('nombre_de_chambres');
+        // dd($request->input('nombre_de_chambres'));
         return view("admin.ajoutbien");
     }
     public function show()
     {
-        $user = Auth::user()->id;
-        // $bien = Propertie::where('user_id',$user->id)->get();
+        // $user = Auth::user()->id;
+        // $user = Auth::user()->id;
         $bien = Propertie::all();
-
-        return view("admin.listebien", compact("bien","user"));
+        return view("admin.listebien", compact("bien"));
     }
     public function store(Request $request)
-    {
-        $request->validate([
+    {   
+            $request->validate([
             "nom" => "required|string|min:3",
             "categorie" => "required|string",
             // "image" => "required|image|max:5000",
-            "multi_image" => "required|image",
+            // "multi_image" => "required|image",
             "description" => "required|string|min:5",
             "dimension_bien" => "required|int",
             "nombre_chambre" => "required|int",
-            "dimension_chambre" => "required|int",
+            // "dimension_chambre" => "required|int",
             "nombre_toillette" => "required|int",
             "balcons" => "required|int",
             "space_vert" => "required",
@@ -62,8 +60,9 @@ class BienController extends Controller
             'date.required' => 'Le champ Date est obligatoire.',
             'date.date' => 'Le champ Date doit être une date valide.',
         ]);
-
+        dd($request);
         $bien = new Propertie();
+        $bien->user_id = 1;
         $bien->nom = $request->nom;
         $bien->categorie = $request->categorie;
         $bien->image = $this->storeImage($request->file('image'));
@@ -78,17 +77,17 @@ class BienController extends Controller
         $bien->date = $request->date;
         $bien->save();
         
-        $images = $request->file('multi_image');
-        $imagePaths = [];
-        foreach ($images as $images) {
-            $imagePath = $this->storeMultiImage($images);
-            $imagePaths[] = $imagePath;
-            $multiImage = new Multi_img();
-            $multiImage->propertie_id = $bien->id; // Associez l'ID de la propriété
-            $multiImage->photo_name = $imagePath;
-            $multiImage->save();
-        }   
-        $bien->dimension_chambre = $request->dimension_chambre;
+        // $images = $request->file('multi_image');
+        // $imagePaths = [];
+        // foreach ($images as $images) {
+        //     $imagePath = $this->storeMultiImage($images);
+        //     $imagePaths[] = $imagePath;
+        //     $multiImage = new Multi_img();
+        //     $multiImage->propertie_id = $bien->id; // Associez l'ID de la propriété
+        //     $multiImage->photo_name = $imagePath;
+        //     $multiImage->save();
+        // }   
+        // $bien->dimension_chambre = $request->dimension_chambre;
         return back()->with('success', 'Votre produit a été ajouter');
     }
     private function storeImage($image): string
@@ -168,12 +167,49 @@ class BienController extends Controller
     }
 
     // Les methodes que j'ai ajouter pour chambres
+    
+    // Ici j'aurais les methodes qui concerne les chambres et ses images 
     public function indexChambre(Request $request)
     {
-        return view('admin.ajoutChambre');
+        // $bien = Propertie::all();
+        $nombre_chambre = $request->nombre_chambre;
+        // dd($nombre_chambre);
+        return view('admin.ajoutChambre' , compact('nombre_chambre'));
     }
     public function storeChambre(Request $request)
     {
-        
+        $request->validate([
+            'dimension_chambre'=> 'required|int',
+            'multi_image'=> 'required|image',
+        ]);
+        $chambre = new Chambre;
+        $chambre->dimension_chambre = $request->dimension_chambre;
+        $chambre->save();
+
+        //pour gerer les images
+        $images = $request->file('multi_image');
+        $imagePaths = [];
+        foreach ($images as $images) {
+            $imagePath = $this->storeMultiImage($images);
+            $imagePaths[] = $imagePath;
+            $multiImage = new Multi_img();
+            // $multiImage->propertie_id = $bien->id; // Associez l'ID de la propriété
+            $multiImage->chambre_id = $chambre->id; // Associez l'ID de la propriété
+            $multiImage->photo_name = $imagePath;
+            $multiImage->save();
+        }   
+        return redirect()->with('success','vous avez ajouter une chambre');
+    }
+
+    public function inputChambre(Request $request){
+        $nombre_de_chambres = $request->input('nombre_de_chambres');
+                            
+        for ($i = 1; $i <= $nombre_de_chambres; $i++) {
+            $chambre = new Chambre();
+            $chambre->nom = $request->input('nom_de_la_chambre_' . $i);
+            $chambre->superficie = $request->input('superficie_de_la_chambre_' . $i);
+            $chambre->save();
+        }
+        return redirect()->back()->with('success','');
     }
 }
